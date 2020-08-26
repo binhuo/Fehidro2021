c405ed2dcc996330e29dfc15175a7cb94f57f095
ArthurRosendo <arthur.rosendo.amaral@gmail.com>
2020-08-26 12:28:45 -0300

    Spring Avaliacao

---
+++ fehidro-api-v2/src/main/java/fehidro/api/controller/AvaliacaoController.java
@@ -0,0 +1,118 @@
+package fehidro.api.controller;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import fehidro.api.model.Avaliacao;
+import fehidro.api.model.CriterioAvaliacao;
+import fehidro.api.model.Proposta;
+import fehidro.api.model.SubcriterioAvaliacao;
+import fehidro.api.model.Usuario;
+import fehidro.api.repository.AvaliacaoRepository;
+import fehidro.model.dto.avaliacao.CadastroAvaliacaoDTO;
+
+@RestController
+@RequestMapping("/avaliacao")
+public class AvaliacaoController {
+	
+	@Autowired
+	private AvaliacaoRepository _avaliacaoRepository;
+		
+	@GetMapping
+	public ResponseEntity<List<CadastroAvaliacaoDTO>> getAll() {
+		List<Avaliacao> list = _avaliacaoRepository.findAll();
+		List<CadastroAvaliacaoDTO> resul = list.stream().map(u -> {return new CadastroAvaliacaoDTO(u);}).collect(Collectors.toList());
+		return ResponseEntity.ok().body(resul);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<CadastroAvaliacaoDTO> get(@PathVariable(value = "id") Long id) {
+		Optional<Avaliacao> aval = _avaliacaoRepository.findById(id);
+		if (aval.isPresent()) {
+			return ResponseEntity.ok(new CadastroAvaliacaoDTO(aval.get()));
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	
+	@GetMapping("/listarAvaliador/{avaliador}")
+	public ResponseEntity<List<CadastroAvaliacaoDTO>> listarAvaliador(@PathVariable(value = "avaliador") Usuario avaliador) {
+		List<Avaliacao> list = _avaliacaoRepository.findAllByAvaliador(avaliador);
+		List<CadastroAvaliacaoDTO> resul = list.stream().map(u -> {return new CadastroAvaliacaoDTO(u);}).collect(Collectors.toList());
+		return ResponseEntity.ok().body(resul);
+	}
+	@GetMapping("/listarCriteiro/{criterio}")
+	public ResponseEntity<List<CadastroAvaliacaoDTO>> listarCriterio(@PathVariable(value = "criterio") CriterioAvaliacao criterio) {
+		List<Avaliacao> list = _avaliacaoRepository.findAllByCriterio(criterio);
+		List<CadastroAvaliacaoDTO> resul = list.stream().map(u -> {return new CadastroAvaliacaoDTO(u);}).collect(Collectors.toList());
+		return ResponseEntity.ok().body(resul);
+	}
+	@GetMapping("/listarSubcriteiro/{subcriterio}")
+	public ResponseEntity<List<CadastroAvaliacaoDTO>> listarSubcriteiro(@PathVariable(value = "subcriterio") SubcriterioAvaliacao subcriterio) {
+		List<Avaliacao> list = _avaliacaoRepository.findAllBySubcriterio(subcriterio);
+		List<CadastroAvaliacaoDTO> resul = list.stream().map(u -> {return new CadastroAvaliacaoDTO(u);}).collect(Collectors.toList());
+		return ResponseEntity.ok().body(resul);
+	}
+	@GetMapping("/listarProposta/{proposta}")
+	public ResponseEntity<List<CadastroAvaliacaoDTO>> listarProposta(@PathVariable(value = "proposta") Proposta proposta) {
+		List<Avaliacao> list = _avaliacaoRepository.findAllByProposta(proposta);
+		List<CadastroAvaliacaoDTO> resul = list.stream().map(u -> {return new CadastroAvaliacaoDTO(u);}).collect(Collectors.toList());
+		return ResponseEntity.ok().body(resul);
+	}
+	
+	@GetMapping("/listarAvaliadorProposta/{proposta}/{avaliador}")
+	public ResponseEntity<List<CadastroAvaliacaoDTO>> listarAvaliadorProposta(@PathVariable(value = "proposta") Proposta proposta,@PathVariable(value = "avaliador") Usuario avaliador ) {
+		List<Avaliacao> list = _avaliacaoRepository.findAllByAvaliadorProposta(proposta,avaliador);
+		List<CadastroAvaliacaoDTO> resul = list.stream().map(u -> {return new CadastroAvaliacaoDTO(u);}).collect(Collectors.toList());
+		return ResponseEntity.ok().body(resul);
+	}
+	@GetMapping("/listarCriterioProposta/{proposta}/{criterio}")
+	public ResponseEntity<List<CadastroAvaliacaoDTO>> listarCriterioProposta(@PathVariable(value = "proposta") Proposta proposta,@PathVariable(value = "criterio") CriterioAvaliacao criterio ) {
+		List<Avaliacao> list = _avaliacaoRepository.findAllByCriterioProposta(proposta,criterio);
+		List<CadastroAvaliacaoDTO> resul = list.stream().map(u -> {return new CadastroAvaliacaoDTO(u);}).collect(Collectors.toList());
+		return ResponseEntity.ok().body(resul);
+	}
+	@GetMapping("/listarSubcriterioProposta/{proposta}/{subcriterio}")
+	public ResponseEntity<List<CadastroAvaliacaoDTO>> listarSubcriterioProposta(@PathVariable(value = "proposta") Proposta proposta,@PathVariable(value = "subcriterio") SubcriterioAvaliacao subcriterio ) {
+		List<Avaliacao> list = _avaliacaoRepository.findAllBySubcriterioProposta(proposta,subcriterio);
+		List<CadastroAvaliacaoDTO> resul = list.stream().map(u -> {return new CadastroAvaliacaoDTO(u);}).collect(Collectors.toList());
+		return ResponseEntity.ok().body(resul);
+	}
+	
+	
+	@PostMapping
+	public ResponseEntity<CadastroAvaliacaoDTO> add(@RequestBody CadastroAvaliacaoDTO avaliacao, UriComponentsBuilder uriBuilder) {
+		
+		try {
+			Avaliacao novo = new Avaliacao(avaliacao);
+			Avaliacao aval = _avaliacaoRepository.save(novo);
+			CadastroAvaliacaoDTO cadastrado = new CadastroAvaliacaoDTO(aval);
+			URI uri = uriBuilder.path("/{id}").buildAndExpand(cadastrado.getId()).toUri();
+			return ResponseEntity.created(uri).body(cadastrado);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
+	}
+	
+	@PutMapping
+	public ResponseEntity<Avaliacao> update(@RequestBody Avaliacao avaliacao) {
+		Avaliacao aval = _avaliacaoRepository.save(avaliacao);	
+		return ResponseEntity.ok(aval);
+	}
+	
+}
---
+++ fehidro-api-v2/src/main/java/fehidro/api/model/Avaliacao.java
@@ -0,0 +1,102 @@
+package fehidro.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import fehidro.model.dto.avaliacao.CadastroAvaliacaoDTO;
+
+@Table(name = "tb_avaliacao")
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Avaliacao extends AbstractEntity {
+	
+	@ManyToOne
+    @JoinColumn(name = "nota_avaliacao_id")
+    private Pontuacao nota;
+	
+    @ManyToOne
+    @JoinColumn(name = "usuario_avaliacao_id")
+    private Usuario avaliador;
+    
+    @ManyToOne
+    @JoinColumn(name = "proposta_avaliacao_id")
+    private Proposta proposta;
+    
+    @ManyToOne
+    @JoinColumn(name = "subcriterio_avaliacao_id")
+    private SubcriterioAvaliacao subcriterio;
+    
+    //TODO: Reconsiderar se necessario
+    @ManyToOne
+    @JoinColumn(name = "criterio_avaliacao_id")
+    private CriterioAvaliacao criterio;
+    
+//    @ManyToOne(cascade=CascadeType.ALL)
+//    @JoinColumn(name = "subPDC_avaliacao_id")
+//    private SubPDC subpdc;
+//    
+//    @ManyToOne(cascade=CascadeType.ALL)
+//    @JoinColumn(name = "pdc_avaliacao_id")
+//    private PDC pdc;
+	
+	public Avaliacao() {
+	}
+	
+	public Avaliacao(CadastroAvaliacaoDTO dto) {
+		if(dto != null) {
+			this.setId(dto.getId());
+			this.nota = dto.getNota();
+			this.avaliador = dto.getAvaliador();
+			this.proposta = dto.getProposta();
+			this.subcriterio = dto.getSubcriterio();
+			this.criterio = dto.getCriterio();
+		}
+	}
+
+	public Pontuacao getNota() {
+		return nota;
+	}
+
+	public void setNota(Pontuacao nota) {
+		this.nota = nota;
+	}
+
+	public Usuario getAvaliador() {
+		return avaliador;
+	}
+
+	public void setAvaliador(Usuario avaliador) {
+		this.avaliador = avaliador;
+	}
+
+	public Proposta getProposta() {
+		return proposta;
+	}
+
+	public void setProposta(Proposta proposta) {
+		this.proposta = proposta;
+	}
+
+	public SubcriterioAvaliacao getSubcriterio() {
+		return subcriterio;
+	}
+
+	public void setSubcriterio(SubcriterioAvaliacao subcriterio) {
+		this.subcriterio = subcriterio;
+	}
+
+	public CriterioAvaliacao getCriterio() {
+		return criterio;
+	}
+
+	public void setCriterio(CriterioAvaliacao criterio) {
+		this.criterio = criterio;
+	}
+	
+	
+
+}
