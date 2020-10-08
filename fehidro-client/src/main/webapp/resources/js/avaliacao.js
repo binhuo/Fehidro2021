4d468dc3f5cbb4d60d57dde8f339cc96d4adae65
ArthurRosendo <arthur.rosendo.amaral@gmail.com>
2020-10-08 10:28:57 -0300

    Inicio javascript comentario avaliacao

--- fehidro-client/src/main/webapp/resources/js/avaliacao.js
+++ fehidro-client/src/main/webapp/resources/js/avaliacao.js
@@ -2,4 +2,11 @@ $(document).ready(function () {
 	$("#formAvaliacao table td label").addClass("custom-control-label");
 	$("#formAvaliacao input[type='checkbox']").addClass("custom-control-input");
 	$("#formAvaliacao td").addClass("custom-control custom-checkbox");
+	
+	document.getElementById("formAvaliacao:txtNota").onchange = function() {
+		print("teste");
+		//	document.getElementById("formAvaliacao:txtComentario").disabled = (txtNota.value == txtNota.lenght);
+	}
+	
 });
+
