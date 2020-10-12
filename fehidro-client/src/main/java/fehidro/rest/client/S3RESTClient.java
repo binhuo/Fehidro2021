3be2bbeeb2ddb3539eadff42c4db5a26f462b21c
Vivian Santos <vivianmilenapds@outlook.com>
2020-10-12 12:00:52 -0300

    Site - Upload S3

--- fehidro-client/src/main/java/fehidro/rest/client/S3RESTClient.java
+++ fehidro-client/src/main/java/fehidro/rest/client/S3RESTClient.java
@@ -1,22 +1,12 @@
 package fehidro.rest.client;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
-import javax.servlet.http.Part;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.Form;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
-import org.glassfish.jersey.media.multipart.BodyPart;
-import org.glassfish.jersey.media.multipart.Boundary;
-import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.glassfish.jersey.media.multipart.MultiPart;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.media.multipart.file.FileDataBodyPart;
@@ -24,26 +14,26 @@ import org.glassfish.jersey.media.multipart.file.FileDataBodyPart;
 
 public class S3RESTClient extends BaseRESTClient {
 	
-	public boolean upload(Part file) {
+	public void upload(File file) {
 		try {
-			MediaType contentType = MediaType.MULTIPART_FORM_DATA_TYPE;
-			contentType = Boundary.addBoundary(contentType);
-			
+			MultiPart multiPart = new MultiPart();
+	        multiPart.setMediaType(MediaType.MULTIPART_FORM_DATA_TYPE);
+	        FileDataBodyPart bodyPart = new FileDataBodyPart("file", file, MediaType.APPLICATION_OCTET_STREAM_TYPE);	        
+	        multiPart.bodyPart(bodyPart);
+	        
 			ClientBuilder.newBuilder()
 				    .register(MultiPartFeature.class)
 				    .build()
 				    .target(REST_WEBSERVICE_URL + REST_AWSS3_URL + "upload")
 					.request()
 					.header(HttpHeaders.AUTHORIZATION, authToken)
-					.post(Entity.entity(file, contentType));
-					
-			return true;
+					.post(Entity.entity(multiPart, multiPart.getMediaType()));
+								
 		} catch(Exception e) {
-			e.printStackTrace();
-			return false;
-		}
+			e.printStackTrace();			
+		}		
 	}
-	
+		
 	public byte[] download(String nomeArquivo) {
 		
 		try {
